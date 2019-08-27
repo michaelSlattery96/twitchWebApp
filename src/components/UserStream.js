aebc9c04322831da5b3832bf381415f889e48f4a
Michael Slattery <michaelslattery16@gmail.com>
2019-08-27 17:32:59 +0100

    Changed class to function and removed pointless code

--- src/components/UserStream.js
+++ src/components/UserStream.js
@@ -1,36 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-const EMBED_URL = 'https://embed.twitch.tv/embed/v1.js';
+function UserStream(props) {
 
-class UserStream extends React.Component {
-  componentDidMount() {
-    let embed;
-    const script = document.createElement('script');
-    script.setAttribute(
-      'src',
-      EMBED_URL
+  const EMBED_URL = 'https://embed.twitch.tv/embed/v1.js';
+
+  const script = document.createElement('script');
+  script.setAttribute(
+    'src',
+    EMBED_URL
+  );
+  script.addEventListener('load', () => {
+    new window.Twitch.Embed("twitch-embed",
+      {
+        width: '1600',
+        height: '800',
+        channel: props.location.state.channel
+      }
     );
-    script.addEventListener('load', () => {
-      embed = new window.Twitch.Embed("twitch-embed",
-        {
-          width: '1600',
-          height: '800',
-          channel: this.props.location.state.channel
-        }
-      );
-    });
-    document.body.appendChild(script);
-  }
+  });
+  document.body.appendChild(script);
 
-  render() {
-    return (
-      <div>
-        Hello {this.props.location.state.channel}
-        <div id={this.props.targetID}>test</div>
-      </div>
-    )
-  }
+  return (
+    <div id={props.targetID}></div>
+  );
 }
 
 UserStream.defaultProps = {
